2b6909111df4976b0a9f221d410f4d282f067a3f
vanathi <123821462+Vanathi2694@users.noreply.github.com>
2023-04-13 12:45:08 +0530

    Update phonepe_pulse.py

--- phonepe_pulse.py
+++ phonepe_pulse.py
@@ -1,128 +1,107 @@
-import json
 import os
+import json
 import pandas as pd
 import streamlit as st
 import mysql.connector
 import plotly.express as px
-import matplotlib.pyplot as plt
-import plotly.graph_objects as go
-dir_path = "C:/GitHub/pulse/data/aggregated/user/country/india"
-st.title("Phonepe pulse Data Visualization and Exploration")
-bgcolor = "#F0F2F6"
-#with st.sidebar:
 
-st.markdown(f"""
-    <style>
-    body {{
-        background-color: {bgcolor};
-    }}
-    </style>
-    """, unsafe_allow_html=True)
+path = "C:/GitHub/pulse/data/top/user/country/india/state"
 
-tab1, tab2, tab3, tab4 = st.tabs(["Load Data", "Explore Data", "Insert Data","Visualization"])
-#load_data_slider_key = 'load_data_slider'
+dict1 = {'State': [], 'Year': [], 'Quarter': [], 'Districts': [], 'Users': []}
 
+@st.cache
+def load_data(path):
+    for state in os.listdir(path):
+        state_dir = os.path.join(path, state)
+        for year in os.listdir(state_dir):
+            year_dir = os.path.join(state_dir, year)
+            for quarter_file in os.listdir(year_dir):
+                quarter_path = os.path.join(year_dir, quarter_file)
+                with open(quarter_path, 'r') as f:
+                    data = json.load(f)
+                    quarter = quarter_file.split('.')[0]
+                    districts = data['data']['districts']
+                    for district in districts:
+                        district_name = district['name']
+                        district_users = district['registeredUsers']
+                        dict1['State'].append(state)
+                        dict1['Year'].append(year)
+                        dict1['Quarter'].append(quarter)
+                        dict1['Districts'].append(district_name)
+                        dict1['Users'].append(district_users)
 
+load_data(path)
 
-def load_data(dir_path):
-    json_files = []
-    for root, dirs, files in os.walk(dir_path):
-        for file in files:
-            if file.endswith('.json'):
-                json_files.append(os.path.join(root, file))
-    dfs = []
-    for json_file in json_files:
-        with open(json_file, 'r') as f:
-            data = json.load(f)
-            usersByDevice = data['data']['usersByDevice']
-            df = pd.DataFrame(usersByDevice)
-            df['filename'] = os.path.basename(json_file)
-            dfs.append(df)
-    final_df = pd.concat(dfs, ignore_index=True)
+df = pd.DataFrame(dict1)
 
-    return final_df
-with tab1:
-    st.header("Data Loaded")
-    df = load_data(dir_path)
+df.to_csv('users_data.csv', index=False)
 
+@st.cache
+def explore_data():
 
-def explore_data(dir_path):
-    df = load_data(dir_path)
-    st.sidebar.subheader('Select a page number:')
-    PAGE_SIZE = 100
-    page = st.sidebar.slider('Page', 1, len(df) // PAGE_SIZE + 1, 1, key='load_data_slider_key_2')
-    start_idx = (page - 1) * PAGE_SIZE
-    end_idx = start_idx + PAGE_SIZE
-    st.write(f'Displaying rows {start_idx + 1} to {min(end_idx, len(df))} of {len(df)}')
-    st.write(df.iloc[start_idx:end_idx])
+    df.dropna(inplace=True)
 
-    st.write('Original Dataframe:')
-    st.dataframe(df)
+    df['Year'] = pd.to_datetime(df['Year'], format='%Y')
 
-    df = df.dropna()
-    num_nulls = df.isnull().sum().sum()
-    st.write(f'Number of null values removed: {num_nulls}')
+    grouped = df.groupby(['State', pd.Grouper(key='Year', freq='Y')])['Users'].sum()
 
-    # Drop duplicate rows based on 'usersByDevice'
-    df = df.drop_duplicates(subset=['count', 'percentage'], keep='first')
-    num_dupes = len(df) - len(df.drop_duplicates(subset=['count', 'percentage']))
-    st.write(f'Number of duplicate rows removed: {num_dupes}')
+    grouped_df = grouped.to_frame().reset_index()
 
-    # Display the final dataframe
-    st.write('Cleaned Dataframe:')
-    st.dataframe(df)
+    pivoted = grouped_df.pivot(index='State', columns='Year', values='Users')
 
+    pivoted.fillna(0, inplace=True)
 
-    return df
+    st.write(pivoted)
 
+    pivoted.to_csv('pivoted_data.csv', index=False)
 
-with tab2:
-    st.header("Data Explored")
-    df = explore_data(dir_path)
-def sqlcon(df):
-    # Connect to the MySQL database
-    mydb = mysql.connector.connect(
-        host="database-phonepe-pulse.cbgdu1nd11gm.ap-south-1.rds.amazonaws.com",
-        user="admin",
-        password="Vanathis",
-        database="database-phonepe-pulse"
-    )
-    mycursor = mydb.cursor()
+explore_data()
 
-    # Create a table if it does not already exist
-    mycursor.execute(
-        "CREATE TABLE IF NOT EXISTS usersByDevice (id INT AUTO_INCREMENT PRIMARY KEY, brand VARCHAR(255), count INT, percentage FLOAT, filename VARCHAR(255))"
-    )
-    rows_to_insert = []
-    # Insert data from the dataframe into the table
-    for index, row in df.iterrows():
-        rows_to_insert.append((row['brand'], row['count'], row['percentage'], row['filename']))
-        if len(rows_to_insert) == 1000:  # Insert in batches of 1000 rows
-            sql = "INSERT INTO usersByDevice (brand, count, percentage, filename) VALUES (%s, %s, %s, %s)"
-            mycursor.executemany(sql, rows_to_insert)
-            rows_to_insert = []
+@st.cache
+def sql_con():
 
-    # Insert any remaining rows
-    if len(rows_to_insert) > 0:
-        sql = "INSERT INTO usersByDevice (brand, count, percentage, filename) VALUES (%s, %s, %s, %s)"
-        mycursor.executemany(sql, rows_to_insert)
-    # Commit changes to the database
-    mydb.commit()
+    pivoted = pd.read_csv('pivoted_data.csv')
 
-    # Print the number of rows inserted
-    st.write(f"{mycursor.rowcount} rows inserted into the 'usersByDevice' table.")
+    conn = mysql.connector.connect(host="database-phonepe-pulse.cbgdu1nd11gm.ap-south-1.rds.amazonaws.com",
+                                   user="admin",
+                                   password="Vanathis",
+                                   database="database-phonepe-pulse")
 
-    # Check if the table exists
-    mycursor.execute(
-        "SELECT * FROM information_schema.tables WHERE table_schema = 'database-phonepe-pulse' AND table_name = 'usersByDevice'"
-    )
-    result = mycursor.fetchone()
+    cursor = conn.cursor()
 
-    if result:
-        st.write("Table 'usersByDevice' exists in the database.")
-    else:
-        st.write("Error: Table 'usersByDevice' does not exist in the database.")
-with tab3:
-    st.header("Data inserted into sql")
-    st.image("https://i1.wp.com/technopoints.co.in/wp-content/uploads/2017/09/data-insert.png?fit=750%2C400&ssl=1", width = 500)
-    sqlcon(df)
+    pivoted = pivoted.astype(int)
+
+    for state, row in pivoted.iterrows():
+        year_2018 = int(row['2018-12-31'])
+        year_2019 = int(row['2019-12-31'])
+        year_2020 = int(row['2020-12-31'])
+        year_2021 = int(row['2021-12-31'])
+        sql = "INSERT INTO users (state, year_2018, year_2019, year_2020, year_2021) VALUES (%s, %s, %s, %s, %s)"
+        values = (state, year_2018, year_2019, year_2020, year_2021)
+        cursor.execute(sql, values)
+
+    conn.commit()
+
+    conn.close()
+
+sql_con()
+@st.cache
+def visualization():
+    with open('C:/GitHub/pulse/india_state_geo.json') as f:
+        data = json.load(f)
+
+    fig = px.choropleth_mapbox(df, geojson=data, locations='State', color='Users',
+                               color_continuous_scale='Viridis', range_color=(0, df['Users'].max()),
+                               mapbox_style='carto-positron', zoom=3, center={'lat': 20.5937, 'lon': 78.9629},
+                               opacity=0.5, labels={'Users': 'Number of Users'})
+    fig.update_layout(margin={'l': 0, 'r': 0, 't': 0, 'b': 0})
+    st.plotly_chart(fig)
+
+    year = st.selectbox('Select a year', df['Year'].unique())
+
+    filtered_data = df[df['Year'] == year]
+
+    fig = px.bar(filtered_data, x='State', y='Users', color='Districts', title=f'Number of Users by State for {year}')
+    st.plotly_chart(fig)
+
+visualization()
